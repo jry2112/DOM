826988dc248aee953cf33af95eeec0672a66832d
jry2112 <jry2112@columbia.edu>
2021-07-28 15:34:59 -0400

    exercises

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,7 @@
+var body = document.lastElementChild.lastElementChild;
+var li3 = body.querySelector("ul").lastElementChild;
+
+// Change Third li text to whatever you want
+li3.innerHTML = "Look at this";
+
+body.querySelector('h1').classList.add("huge");
